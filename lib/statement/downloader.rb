42689a168e0f3dca8ff0823155504c34926ee978
Nathan Lee <nathan@globalphobia.com>
2015-06-22 22:55:19 -0600

    Initial Commit

---
+++ lib/statement/downloader.rb
@@ -0,0 +1,7 @@
+require "statement/downloader/version"
+
+module Statement
+  module Downloader
+    # Your code goes here...
+  end
+end
---
+++ lib/statement/downloader/version.rb
@@ -0,0 +1,5 @@
+module Statement
+  module Downloader
+    VERSION = "0.0.1"
+  end
+end
