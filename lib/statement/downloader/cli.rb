c3390226b0663c9962047b81c2b31cccb8030373
Nathan Lee <nathan@globalphobia.com>
2015-06-27 23:06:08 -0600

    Connect to url

--- lib/statement/downloader/cli.rb
+++ lib/statement/downloader/cli.rb
@@ -1,8 +1,14 @@
 require "thor"
+require "faraday"
 
 class Statement::Downloader::CLI < Thor
   desc "get URL", "URL to pull from"
   def get(url)
     puts "Connecting to: #{url}"
+
+    conn = Faraday.new(:url => url)
+    response = conn.get
+
+    puts response.body
   end
 end
